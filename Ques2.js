061ebf4b0739566ac13b69fa4d3ac54ce2b69511
Abhinav010G <111418688+Abhinav010G@users.noreply.github.com>
2022-08-22 17:30:45 +0530

    Delete Ques2.js

--- Ques2.js
+++
@@ -1,5 +0,0 @@
-// Write a JavaScript program to convert temperatures to and from Celsius to Fahrenheit.
-
-const c = +prompt(" Enter the temperature in celsius  ");
-const Fahrenheit = (c*9+32*5)/5;
-console.log(`${c} degree celsius = ${Fahrenheit} fahernhit`);
