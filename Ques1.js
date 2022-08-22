f87b9d1d7d751b8094b487bc3b982a8f77ba1bef
Abhinav010G <111418688+Abhinav010G@users.noreply.github.com>
2022-08-22 17:30:17 +0530

    Delete Ques1.js

--- Ques1.js
+++
@@ -1,8 +0,0 @@
-// 1. Write a program to find whether a given year is a leap year or not.
- let year = prompt("Enter the year, here!");
-
- if (year%4===0  && year%100!==0 || year%400===0){
-    console.log(`The year ${year} is an leap year`);
- }else{
-    console.log(`The year ${year} is not an leap year`);
- }
